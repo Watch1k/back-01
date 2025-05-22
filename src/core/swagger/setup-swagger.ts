2ecf3d7eb4090d5f038f11a99f13f002a68daaee
Oleksandr Filshyn <host.sf@gmail.com>
2025-05-22 23:57:11 +0300

    save

--- src/core/swagger/setup-swagger.ts
+++ src/core/swagger/setup-swagger.ts
@@ -1,21 +1,33 @@
-import swaggerJsdoc from 'swagger-jsdoc';
-import swaggerUi from 'swagger-ui-express';
+import swaggerUI from 'swagger-ui-express';
+import swaggerJsDoc from 'swagger-jsdoc';
 import { Express } from 'express';
+import express from 'express';
 
-const swaggerOptions = {
+const options = {
   definition: {
     openapi: '3.0.0',
     info: {
-      title: 'Uber API',
+      title: 'Your API',
       version: '1.0.0',
-      description: 'uber API',
+      description: 'Your API Description',
     },
+    servers: [
+      {
+        url: '/',
+        description: 'Current server',
+      },
+    ],
   },
-  apis: ['./src/**/*.swagger.yml'],
+  apis: ['./src/**/*.ts'],
 };
 
-const swaggerSpec = swaggerJsdoc(swaggerOptions);
+const swaggerSpec = swaggerJsDoc(options);
 
 export const setupSwagger = (app: Express) => {
-  app.use('/api', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+  // Serve Swagger UI static files
+  const swaggerUiAssetPath = require('swagger-ui-express').absolutePath();
+  app.use('/api', express.static(swaggerUiAssetPath));
+
+  // Setup Swagger UI
+  app.use('/api', swaggerUI.serve, swaggerUI.setup(swaggerSpec));
 };
