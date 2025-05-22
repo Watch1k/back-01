830f1a18847536ccf98d64341ed6f65af20d2333
Oleksandr Filshyn <host.sf@gmail.com>
2025-05-22 23:55:05 +0300

    save

--- src/core/swagger/setup-swagger.ts
+++ src/core/swagger/setup-swagger.ts
@@ -2,26 +2,19 @@ import swaggerJsdoc from 'swagger-jsdoc';
 import swaggerUi from 'swagger-ui-express';
 import { Express } from 'express';
 
-const options = {
+const swaggerOptions = {
   definition: {
     openapi: '3.0.0',
     info: {
-      title: 'Your API',
+      title: 'Uber API',
       version: '1.0.0',
-      description: 'Your API Description',
+      description: 'uber API',
     },
-    servers: [
-      {
-        url: process.env.VERCEL_URL
-          ? `https://${process.env.VERCEL_URL}`
-          : 'http://localhost:3000',
-      },
-    ],
   },
   apis: ['./src/**/*.swagger.yml'],
 };
 
-const swaggerSpec = swaggerJsdoc(options);
+const swaggerSpec = swaggerJsdoc(swaggerOptions);
 
 export const setupSwagger = (app: Express) => {
   app.use('/api', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
