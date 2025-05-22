e17f5ed3c5d5b09fa9b8ba4b9cb417cd09d986f8
Oleksandr Filshyn <host.sf@gmail.com>
2025-05-22 23:58:17 +0300

    save

--- src/core/swagger/setup-swagger.ts
+++ src/core/swagger/setup-swagger.ts
@@ -1,33 +1,28 @@
 import swaggerUI from 'swagger-ui-express';
 import swaggerJsDoc from 'swagger-jsdoc';
 import { Express } from 'express';
-import express from 'express';
 
 const options = {
   definition: {
     openapi: '3.0.0',
     info: {
-      title: 'Your API',
+      title: 'Videos API',
       version: '1.0.0',
       description: 'Your API Description',
     },
     servers: [
       {
-        url: '/',
-        description: 'Current server',
+        url: process.env.VERCEL_URL
+          ? `https://${process.env.VERCEL_URL}`
+          : 'http://localhost:3000',
       },
     ],
   },
-  apis: ['./src/**/*.ts'],
+  apis: ['./src/**/*.swagger.yml'],
 };
 
 const swaggerSpec = swaggerJsDoc(options);
 
 export const setupSwagger = (app: Express) => {
-  // Serve Swagger UI static files
-  const swaggerUiAssetPath = require('swagger-ui-express').absolutePath();
-  app.use('/api', express.static(swaggerUiAssetPath));
-
-  // Setup Swagger UI
   app.use('/api', swaggerUI.serve, swaggerUI.setup(swaggerSpec));
 };
